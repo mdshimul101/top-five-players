8cceba5cd54222a36bc1ee8783904c36c130121a
MD SHIMUL <80319580+mdshimul101@users.noreply.github.com>
2022-08-23 02:59:33 +0600

    complete player list

--- Js/player-list.js
+++ Js/player-list.js
@@ -1,27 +1,23 @@
 
 document.getElementById('player-1-btn').addEventListener('click', function () {
-    setPlayer('player-1-name', 'player-list');
-    buttonDisible('player-1-btn');
+    setPlayer('player-1-name', 'player-1-btn');
 })
 document.getElementById('player-2-btn').addEventListener('click', function () {
-    setPlayer('player-2-name', 'player-list');
-    buttonDisible('player-2-btn');
+    setPlayer('player-2-name', 'player-2-btn');
+
 })
 document.getElementById('player-3-btn').addEventListener('click', function () {
-    setPlayer('player-3-name', 'player-list');
-    buttonDisible('player-3-btn');
+    setPlayer('player-3-name', 'player-3-btn');
 })
 document.getElementById('player-4-btn').addEventListener('click', function () {
-    setPlayer('player-4-name', 'player-list');
-    buttonDisible('player-4-btn');
+    setPlayer('player-4-name', 'player-4-btn');
 })
 document.getElementById('player-5-btn').addEventListener('click', function () {
-    setPlayer('player-5-name', 'player-list');
-    buttonDisible('player-5-btn');
+    setPlayer('player-5-name', 'player-5-btn');
 })
 document.getElementById('player-6-btn').addEventListener('click', function () {
-    setPlayer('player-6-name', 'player-list');
-    buttonDisible('player-6-btn');
+    setPlayer('player-6-name', 'player-6-btn');
+
 })
 
 //Button Disible function
@@ -30,48 +26,37 @@ function buttonDisible(buttonId) {
     document.getElementById(buttonId).style.backgroundColor = "rgb(148 163 184)";
 }
 
-
 const playerArray = [];
 
-function setPlayer(playerId, listId) {
-    const li = document.createElement('li');
-    li.innerText = document.getElementById(playerId).innerText;
-    const playerName = li.innerText;
-    const playerObject = {
-        playerName: playerName
-    };
-    playerArray.push(playerObject);
+function setPlayer(playerId, selectButton) {
+
+
+    const playerName = document.getElementById(playerId).innerText;
+
+    playerArray.push(playerName);
     const totalPlayer = playerArray.length;
     if (totalPlayer > 5) {
         alert("You could not add player");
         return;
     }
+    const tableBody = document.getElementById('players');
+    tableBody.innerHTML = "";
+    for (let i = 0; i < totalPlayer; i++) {
+        const name = playerArray[i];
+
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+               <th>${i + 1}.</th>
+               <td>${name}</td>
+            `;
+
+        tableBody.appendChild(tr);
+
+    }
+
+    buttonDisible(selectButton);
     document.getElementById('total-player').innerText = totalPlayer;
-    console.log(playerArray.length);
-    document.getElementById(listId).appendChild(li);
-
-
-    // const myNodelist = document.querySelectorAll("#player-list li");
-    // const a = myNodelist.length;
-    // console.log(a);
-
 
 
 }
 
-
-
-
-/*
-const playerArray1 = [];
-function addPlayer(element) {
-    const playerName = element.parentNode.children[0].innerText;
-    const playerObject = {
-        playerName: playerName
-    };
-    playerArray1.push(playerObject);
-    console.log(playerArray1.length);
-
-
-}
-*/
