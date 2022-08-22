dd18f4096bfabd46aeb77884eb8613a520a731af
MD SHIMUL <80319580+mdshimul101@users.noreply.github.com>
2022-08-23 00:12:32 +0600

    set button disible

--- Js/player-list.js
+++ Js/player-list.js
@@ -1,18 +1,36 @@
 
 document.getElementById('player-1-btn').addEventListener('click', function () {
     setPlayer('player-1-name', 'player-list');
-    document.getElementById("player-1-btn").disabled = true;
-
-
+    buttonDisible('player-1-btn');
 })
 document.getElementById('player-2-btn').addEventListener('click', function () {
     setPlayer('player-2-name', 'player-list');
-    document.getElementById("player-1-btn").disabled = true;
+    buttonDisible('player-2-btn');
 })
 document.getElementById('player-3-btn').addEventListener('click', function () {
     setPlayer('player-3-name', 'player-list');
-    document.getElementById("player-1-btn").disabled = true;
+    buttonDisible('player-3-btn');
 })
+document.getElementById('player-4-btn').addEventListener('click', function () {
+    setPlayer('player-4-name', 'player-list');
+    buttonDisible('player-4-btn');
+})
+document.getElementById('player-5-btn').addEventListener('click', function () {
+    setPlayer('player-5-name', 'player-list');
+    buttonDisible('player-5-btn');
+})
+document.getElementById('player-6-btn').addEventListener('click', function () {
+    setPlayer('player-6-name', 'player-list');
+    buttonDisible('player-6-btn');
+})
+
+//Button Disible function
+function buttonDisible(buttonId) {
+    document.getElementById(buttonId).disabled = true;
+    document.getElementById(buttonId).style.backgroundColor = "rgb(148 163 184)";
+}
+
+
 const playerArray = [];
 
 function setPlayer(playerId, listId) {
@@ -24,10 +42,18 @@ function setPlayer(playerId, listId) {
     };
     playerArray.push(playerObject);
     const totalPlayer = playerArray.length;
+    if (totalPlayer > 5) {
+        alert("You could not add player");
+        return;
+    }
     document.getElementById('total-player').innerText = totalPlayer;
     console.log(playerArray.length);
     document.getElementById(listId).appendChild(li);
 
+
+    // const myNodelist = document.querySelectorAll("#player-list li");
+    // const a = myNodelist.length;
+    // console.log(a);
 
 
 
